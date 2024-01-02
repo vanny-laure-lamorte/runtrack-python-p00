07eb722bfd2ae8b7cb274e39dd7ae805e19f643f
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-02 22:24:02 +0100

    Modifier def afficherInfos

--- jour01/job08/job08.py
+++ jour01/job08/job08.py
@@ -4,32 +4,29 @@ class Cercle:
         self.donnee_rayon = rayon
                
     def changerRayon(self, new_rayon):
-        print("Le nouveau rayon est", new_rayon)
-        self.donnee_rayon = new_rayon     
-        self.circonference()
-        self.aire()
-        self.diametre()         
+        self.donnee_rayon = new_rayon  
+        return new_rayon   
 
     def afficherInfos(self):
-        print ("Le rayon du cercle est: ", self.donnee_rayon )
+        print("Le rayon du cercle est: ", self.donnee_rayon)
+        print ("La circonference du cercle est : ", self.circonference())
+        print ("L'aire du cercle est : ", self.aire())
+        print ("Le diamètre du cercle est : ", self.diametre() ) 
 
     def circonference(self):
         resultat_circonference = 2 * self.donnee_rayon * 3.14
-        print ("La circonference du cercle est :  ", resultat_circonference)
-
+        return resultat_circonference
+        
     def aire(self):
         resultat_aire = 3.14 * self.donnee_rayon * self.donnee_rayon
-        print ("L'aire du cercle est : ", resultat_aire)
+        return resultat_aire        
 
     def diametre(self):
         resultat_diametre = self.donnee_rayon*2
-        print ("Le diamètre du cercle est : ", resultat_diametre) 
+        return resultat_diametre        
 
 cercle1 = Cercle(4)
 cercle1.afficherInfos()
-cercle1.circonference()
-cercle1.aire()
-cercle1.diametre()
 
 cercle2 = Cercle(7)
 cercle2.changerRayon(7)
