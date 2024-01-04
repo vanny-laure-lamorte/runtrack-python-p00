6537c4d5f925e1830b17ff3536200857cf6b9472
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-04 09:22:28 +0100

    job02

--- jour02/job02.py
+++ jour02/job02.py
@@ -32,12 +32,12 @@ print (f"Le titre du livre : {info_livre.get_titre()}\n L'auteur du livre : {inf
 # Exemple pour afficher un message erreur si le nombre de page est en chiffre décimale.
 info_livre.set_titre("Thérèse Raquin")
 info_livre.set_auteur("Émile Zola")
-info_livre.set_nb_page(288.5)
 print (f"\n Le titre du livre:{info_livre.get_titre()}\n L'auteur du livre: {info_livre.get_auteur()}\n Le nombre de page: {info_livre.get_nb_page()}")
+info_livre.set_nb_page(288.5)
 
 # Exemple pour afficher un message erreur si le nombre de page en négatif
 info_livre.set_titre("L'Odyssée")
 info_livre.set_auteur("Homère")
-info_livre.set_nb_page(-162)
 print (f"\n Le titre du livre:{info_livre.get_titre()}\n L'auteur du livre: {info_livre.get_auteur()}\n Le nombre de page: {info_livre.get_nb_page()}")
+info_livre.set_nb_page(-162)
 
